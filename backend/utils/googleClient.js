e1cea0b1bd0b55cf2c9c199a4846763c14dbe55a
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2026-01-13 16:30:54 +0530

    googleclient.js file updated in backend

--- backend/utils/googleClient.js
+++ backend/utils/googleClient.js
@@ -2,10 +2,11 @@ const { google } = require('googleapis');
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI
 
 exports.oauth2Client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI,
+    GOOGLE_REDIRECT_URI,
     'postmessage'
 );
